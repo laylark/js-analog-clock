77f98822ab5645c5c3cddc0b8202cedbd2ca7200
Layla Krauss <laylan@gmail.com>
2022-09-12 13:55:04 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const secondsHand = document.getElementById('seconds-hand');
+const minutesHand = document.getElementById('minutes-hand');
+const hoursHand = document.getElementById('hours-hand');
+const ticks = document.getElementById('ticks');
+
+function drawTicks() {
+    for (let i = 0; i < 12; i++) {
+        ticks.style.transform = 'rotate(30deg)';
+        
+    }
+}
+
+function getTime() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
+    const timeInterval = 6;
+
+    secondsHand.style.transform = 'rotate(' + (seconds * timeInterval) + 'deg)';
+    minutesHand.style.transform = 'rotate(' + (minutes * timeInterval + seconds / 10) + 'deg)';
+    hoursHand.style.transform = 'rotate(' + (hours * 30 + minutes / 2) + 'deg)';
+}
+
+setInterval(getTime, 100);
